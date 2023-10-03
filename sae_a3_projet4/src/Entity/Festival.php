2013f6acdd39c0c4327190c625eb48bea84c3642
Loïs <lois.odiardo@gmail.com>
2023-10-03 04:06:42 +0200

    Objet Festival et méthode changemnet d'état d'un festival

---
+++ sae_a3_projet4/src/Entity/Festival.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FestivalRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FestivalRepository::class)]
+class Festival
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $identifiant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $validation = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $lieux = [];
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $postes = [];
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $debut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdentifiant(): ?int
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant(int $identifiant): static
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isValidation(): ?bool
+    {
+        return $this->validation;
+    }
+
+    public function setValidation(bool $validation): static
+    {
+        $this->validation = $validation;
+
+        return $this;
+    }
+
+    public function validation()
+    {
+        $this->validation = true;
+    }
+
+    public function getLieux(): array
+    {
+        return $this->lieux;
+    }
+
+    public function setLieux(array $lieux): static
+    {
+        $this->lieux = $lieux;
+
+        return $this;
+    }
+
+    public function getPostes(): array
+    {
+        return $this->postes;
+    }
+
+    public function setPostes(array $postes): static
+    {
+        $this->postes = $postes;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): static
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): static
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+}
